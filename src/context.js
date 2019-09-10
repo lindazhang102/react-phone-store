1e8ef8363c73f50149c9df985c7fb45cabbd1704
apple <lindazhang102@gmail.com>
2019-09-10 18:08:39 +0800

    code productlist page

---
+++ src/context.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { storeProducts, detailProduct} from './data'
+
+const ProductContext = React.createContext();
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+    state = {
+        products: [],
+        detailProduct: detailProduct
+    }
+    componentDidMount(){
+        this.setProducts();
+    }
+    setProducts = () =>{
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = { ...item };
+            tempProducts = [...tempProducts,singleItem];
+        })
+        this.setState(()=>{
+            return { products: tempProducts };
+        })
+    }
+    handleDetail = () => {
+        console.log('hello from detail');
+    }
+    addToCart = () => {
+        console.log('hello from add to cart');
+    }
+    tester = () =>{
+        console.log('state products:', this.state.products[0].inCart);
+        console.log('data products:', storeProducts[0].inCart);
+        const tempProducts = [...this.state.products];
+        tempProducts[0].inCart = true;
+        this.setState(()=>{
+            return {products:tempProducts},()=>{
+                console.log('state products:', this.state.products[0].inCart);
+                console.log('data products:', storeProducts[0].inCart);
+            }
+        })
+    }
+    render() {
+        return (
+            <ProductContext.Provider value={{...this.state,handleDetail:this.handleDetail,addToCart:this.addToCart}}>
+                {/* <button onClick={this.tester}>test me</button> */}
+                {this.props.children}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider,ProductConsumer};
